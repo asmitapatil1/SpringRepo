2e954754bd696a5b584f3931cbc4246673cf9cd3
Asmita Patil <47376800+asmitapatil16@users.noreply.github.com>
2019-04-04 12:09:25 +0530

    Spring aop

---
+++ comspringaop/src/main/java/com/comspringaop/ComspringaopApplication.java
@@ -0,0 +1,13 @@
+package com.comspringaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComspringaopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComspringaopApplication.class, args);
+	}
+
+}
